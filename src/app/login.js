6d45ed5275288f1b72b91e6b7be7fd0f811e7f1c
John Sheehy <sheehy.john@gmail.com>
2021-01-22 13:00:35 +0000

    auto indent

--- src/app/login.js
+++ src/app/login.js
@@ -2,27 +2,27 @@ import React from "react"
 import { navigate } from "gatsby"
 
 import {
-  IdentityModal,
-  useIdentityContext,
+    IdentityModal,
+    useIdentityContext,
 } from "react-netlify-identity-widget"
-import "react-netlify-identity-widget/styles.css" // delete if you want to bring your own CSS
+//import "react-netlify-identity-widget/styles.css" // delete if you want to bring your own CSS
 
 function Login() {
-  const identity = useIdentityContext()
-  const [dialog, setDialog] = React.useState(false)
-  return (
-    <>
-      <h1>Log in</h1>
-      <button onClick={() => setDialog(true)}>log in</button>
+    const identity = useIdentityContext()
+    const [dialog, setDialog] = React.useState(false)
+    return (
+        <>
+            <h1>Log in</h1>
+            <button onClick={() => setDialog(true)}>log in</button>
 
-      <IdentityModal
-        showDialog={dialog}
-        onCloseDialog={() => setDialog(false)}
-        onLogin={user => navigate("/app/profile")}
-        onSignup={user => navigate("/app/profile")}
-      />
-    </>
-  )
+            <IdentityModal
+                showDialog={dialog}
+                onCloseDialog={() => setDialog(false)}
+                onLogin={user => navigate("/app/profile")}
+                onSignup={user => navigate("/app/profile")}
+            />
+        </>
+    )
 }
 
 export default Login
