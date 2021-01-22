6cd5de47b124630c3f6806356ac1f8dd7c334c8d
John Sheehy <sheehy.john@gmail.com>
2021-01-22 11:52:15 +0000

    auto indent

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -4,44 +4,48 @@ import { Link, navigate } from "gatsby"
 import { useIdentityContext } from "react-netlify-identity-widget"
 
 export default () => {
-  const { user, isLoggedIn, logoutUser } = useIdentityContext()
-  let message = isLoggedIn
-    ? `Hello, ${user.user_metadata && user.user_metadata.full_name}`
-    : "You are not logged in"
+    const { user, isLoggedIn, logoutUser } = useIdentityContext()
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flex: "1",
-        justifyContent: "space-between",
-        borderBottom: "1px solid #d1c1e0",
-        backgroundColor: "aliceblue",
-      }}
-    >
-      <span>{message}</span>
+    //console.log('isLoggedIn = ' + isLoggedIn);
+    console.log("%cisLoggedIn = %o", "color: red ;", isLoggedIn);
 
-      <nav>
-        <span>Navigate the app: </span>
-        <Link to="/app/">Main</Link>
-        {` `}
-        <Link to="/app/profile">Profile</Link>
-        {` `}
-        {isLoggedIn ? (
-          <a
-            href="/"
-            onClick={async event => {
-              event.preventDefault()
-              await logoutUser()
-              navigate(`/app/login`)
+    let message = isLoggedIn
+        ? `Hello, ${user.user_metadata && user.user_metadata.full_name}`
+        : "You are not logged in"
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                flex: "1",
+                justifyContent: "space-between",
+                borderBottom: "1px solid #d1c1e0",
+                backgroundColor: "aliceblue",
             }}
-          >
-            Logout
-          </a>
-        ) : (
-          <Link to="/app/login">Login</Link>
-        )}
-      </nav>
-    </div>
-  )
+        >
+            <span>{message}</span>
+
+            <nav>
+                <span>Navigate the app: </span>
+                <Link to="/app/">Main</Link>
+                {` `}
+                <Link to="/app/profile">Profile</Link>
+                {` `}
+                {isLoggedIn ? (
+                    <a
+                        href="/"
+                        onClick={async event => {
+                            event.preventDefault()
+                            await logoutUser()
+                            navigate(`/app/login`)
+                        }}
+                    >
+                        Logout
+                    </a>
+                ) : (
+                        <Link to="/app/login">Login</Link>
+                    )}
+            </nav>
+        </div>
+    )
 }
