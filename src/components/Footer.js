8908050e24ce35b779d6e1e9b8d67dd0d8828742
John Sheehy <sheehy.john@gmail.com>
2021-03-25 22:31:06 +0000

    twitter footer to john@c4c

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default () => (
     <div>
         <h2 className="taCenter">
             Follow {' '}
-            <a href="https://twitter.com/Code4Creativity" >john@c4c</a> on Twitter
+            <a href="https://twitter.com/Code4Creativity" target="_blank">john@c4c</a> on Twitter
         </h2>
         <br />
         <footer className="footer">
